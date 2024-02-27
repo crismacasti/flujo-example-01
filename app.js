5e9102f47b3ea93bd66c0816c36bd3d516562974
crismacasti <cmcasti96@gmail.com>
2024-02-27 11:59:55 -0500

    create function getUsername

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ function saludoRandom(saludos) {
   const random = Math.floor(Math.random() * saludos.length);
   return saludos[random];
 }
+
+const users = [
+  {
+    username: "pepito123",
+  },
+  {
+    username: "pedro01",
+  },
+];
+
+function getUsername(username) {
+  const user = users.filter((user) => user.username === username);
+  return user[0] || "Not found";
+}
+
+console.log(getUsername("pedro01"));
